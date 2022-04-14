0d7547067a0c473496f2c32f4de13a12720f9481
Marta Gajewska <gajewska.marta16@gmail.com>
2022-04-14 14:53:47 +0200

    add navigation

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburgerConatiner = document.querySelector('#header__main-nav');
+const hamburger = document.querySelector('.header__main-nav--hamburger');
+const links = document.querySelectorAll('.header__main-nav--links li');
+
+hamburger.addEventListener('click', () => {
+  hamburgerConatiner.classList.toggle('clicked');
+  links.forEach((link) => {
+    link.classList.toggle('fade');
+  });
+});
